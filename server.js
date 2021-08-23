e1e9722a2760524d96260abb5d88e2d29f2b0702
Finn Stainton <finnstainton@gmail.com>
2021-08-23 20:07:13 +1200

    Added JWT/local auth

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const db = require('./db')
-const path = require("path")
-const http = require("http")
-require('dotenv').config()
+const app = require("./app");
+const PORT = process.env.PORT || 5678;
 
-// Import Routers
-const userRouter = require('./routes/userRouter')
-const indexRouter = require('./routes/indexRouter')
-
-const app = express()
-const PORT = process.env.PORT || 3000
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
-
-// Set up DB
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
-// Use Routers
-app.use('/', indexRouter)
-app.use('/user', userRouter)
-
-const server = http.createServer(app);
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
