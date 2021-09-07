46f68da4c351b31a04d8b1f7f5e86a6166c36c76
billymcd <billymcdowd@gmail.com>
2021-09-08 10:52:12 +1200

    Add Listing Routes
    Added listing routes and model. Having difficulty getting front end to connect to back end correctly however.

---
+++ listings/listing.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ListingSchema = new Schema({
+  // flatID: { type: String, required: true },
+  description: { type: String, required: true },
+  utilities: { type: String, required: false },
+  rent: { type: Number, required: true },
+  rentUnits: { type: String, required: true },
+  roomAvailable: { type: Date, required: true }
+});
+
+ListingSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+      delete ret._id;
+      delete ret.hash;
+  }
+});
+
+module.exports = mongoose.model('Listing', ListingSchema);
---
+++ listings/listing.service.js
@@ -0,0 +1,63 @@
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const Listing = db.Listing;
+
+module.exports = {
+    // getAll,
+    getOwned,
+    getById,
+    addListing,
+    update,
+    delete: _delete
+};
+
+// async function getAll() {
+//     return await Listing.find();
+// }
+
+async function getOwned() {
+    return await Listing.find();
+}
+
+async function getById(id) {
+    return await Listing.findById(id);
+}
+
+async function addListing(listingparam) {
+    // const flatID = listingparam.flatID;
+    const description = listingparam.description;
+    const utilities = listingparam.utilities;
+    const rent = Number(listingparam.rent);
+    const rentUnits = listingparam.rentUnits;
+    const roomAvailable = Date.parse(listingparam.roomAvailable);
+
+    const newListing = new Listing({
+        // flatID,
+        description,
+        utilities,
+        rent,
+        rentUnits,
+        roomAvailable
+    });
+
+    return await newListing.save();
+}
+
+async function update(id, listingparam) {
+    const Listing = await Listing.findById(id)
+        .then(listing => {
+            listing.description = listingparam.description;
+            listing.utilities = listingparam.utilities;
+            listing.rent = Number(listingparam.rent);
+            listing.rentUnits = listingparam.rentUnits;
+            listing.roomAvailable = Date.parse(listingparam.roomAvailable);
+        });
+    
+    return await Listing.save();
+}
+
+async function _delete(id) {
+    await Listing.findByIdAndRemove(id);
+}
