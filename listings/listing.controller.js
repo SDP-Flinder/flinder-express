932c959fdb43f29d00739ef193f96575395c8a7f
prunellaparisa <prunella.poiney@gmail.com>
2021-09-17 18:19:36 +1200

    updated listing feature

--- listings/listing.controller.js
+++ listings/listing.controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const listingService = require('./listing.service');
 const authorize = require('_helpers/authorize')
-const Role = require('_helpers/role');
+const Role = require('../_helpers/role');
 
 // routes
 router.post('/add', authorize(Role.Flat), addListing)
